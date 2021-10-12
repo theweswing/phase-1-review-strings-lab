deaec6f861bca70483c74f21cce9a1112e6fa22f
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-10-12 16:42:30 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortName = currentUser.slice(0,1)
+const shortGreeting = `Welcome, ${shortName}!`;
+
